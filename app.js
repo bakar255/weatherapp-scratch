1062690077588e67900ae3849986f77addd2e6c5
OfOluo <aboubakari.camarargy@gmail.com>
2025-11-21 21:25:51 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,25 @@
+
+    
+    const URL = "https://api.open-meteo.com/v1/forecast?latitude=48.85&longitude=2.35&current_weather=true"; // API URL
+
+    fetch(URL)
+    .then(response => { // Response de l'API 
+        console.log("Response : ", response); //
+        return response.json() // On unwrap le packet json        
+    })
+
+   .then(data => { 
+         console.log(data);
+         const meteo = data.current_weather; 
+ 
+          const Weather = document.getElementById("temperature");
+          Weather.textContent = `${meteo.temperature}  °C`;
+          document.getElementById("windSpeed").textContent = `${meteo.windspeed} km/h`
+          document.getElementById("windDirection").textContent = `${meteo.windirection}km/h`
+    })
+    .catch(error => {
+
+        Weather.textContent = "Données indisponible";
+        console.error("Erreur:", error);
+    });
+
